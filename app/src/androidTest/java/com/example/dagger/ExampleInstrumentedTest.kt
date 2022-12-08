6ac45500e5fd59b6bc2fe0802ac36c1c31929de4
YoshuaAditya <yoshuaadityakurnia@gmail.com>
2022-12-08 08:28:53 +0700

    Add instrument test
    Using espresso

--- app/src/androidTest/java/com/example/dagger/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/dagger/ExampleInstrumentedTest.kt
@@ -1,5 +1,11 @@
 package com.example.dagger
 
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -13,12 +19,43 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
+
 class ExampleInstrumentedTest {
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.dagger", appContext.packageName)
+    fun runApp() {
+        ActivityScenario.launch(MainActivity::class.java)
+        onView(withText("Available Commands:")).check(matches(isDisplayed()))
+        //Hello command
+        onView(withText("HELLO")).perform(click())
+        onView(withText("SUBMIT")).perform(click())
+        onView(withText("Hello World!!")).check(matches(isDisplayed()))
+        //Deposit command before login
+        onView(withText("DEPOSIT")).perform(click())
+        onView(withText("SUBMIT")).perform(click())
+        onView(withText("Please login first")).check(matches(isDisplayed()))
+        //Login command
+        onView(withText("LOGIN")).perform(click())
+        onView(withText("SUBMIT")).perform(click())
+        onView(withText("user is logged in.")).check(matches(isDisplayed()))
+        //Withdraw command before deposit
+        onView(withText("WITHDRAW")).perform(click())
+        onView(withText("SUBMIT")).perform(click())
+        onView(withText("Balance is not enough")).check(matches(isDisplayed()))
+        //Deposit command
+        onView(withText("DEPOSIT")).perform(click())
+        onView(withText("SUBMIT")).perform(click())
+        onView(withText("user now has: 100")).check(matches(isDisplayed()))
+        //Deposit command again
+        onView(withText("DEPOSIT")).perform(click())
+        onView(withText("SUBMIT")).perform(click())
+        onView(withText("user now has: 200")).check(matches(isDisplayed()))
+        //Withdraw command
+        onView(withText("WITHDRAW")).perform(click())
+        onView(withText("SUBMIT")).perform(click())
+        onView(withText("user balance after withdraw: 160")).check(matches(isDisplayed()))
+        //Logout command
+        onView(withText("LOGOUT")).perform(click())
+        onView(withText("SUBMIT")).perform(click())
+        onView(withText("user log out successfully.")).check(matches(isDisplayed()))
     }
 }
